14a7c9fd8cddf84a272f71c4a898cf122a253ac5
abirhossain <amee11315@gmail.com>
2018-08-19 01:08:50 +0600

    Initial commit

---
+++ imagefromlayout/src/main/java/com/imagefromlayout/abiramee/imagefromlayout/Format.java
@@ -0,0 +1,6 @@
+package com.imagefromlayout.abiramee.imagefromlayout;
+
+public interface Format {
+    public static final String PNG = ".png";
+    public static final String JPG = ".jpg";
+}
---
+++ imagefromlayout/src/main/java/com/imagefromlayout/abiramee/imagefromlayout/ImageFromLayout.java
@@ -0,0 +1,71 @@
+package com.imagefromlayout.abiramee.imagefromlayout;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageFromLayout {
+
+//    private View mView;
+//    private Context mContext;
+//    private Bitmap mBitmap;
+
+    public ImageFromLayout() {
+
+    }
+
+    public static Bitmap getBitmap(Context context, View view) {
+        view.setDrawingCacheEnabled(true);
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache(true);
+
+        return Bitmap.createBitmap(view.getDrawingCache());
+    }
+
+    public static String saveImage(Context context, View view, String fileName, String imageFormat) throws IOException {
+        view.setDrawingCacheEnabled(true);
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache(true);
+
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File dir = new File(filePath);
+        if(!dir.exists())
+            dir.mkdirs();
+        File file = new File(dir, fileName + imageFormat);
+        FileOutputStream fOut = new FileOutputStream(file);
+
+        Bitmap.createBitmap(view.getDrawingCache()).compress(Bitmap.CompressFormat.PNG, 100, fOut);
+        fOut.flush();
+        fOut.close();
+
+        return file.getAbsolutePath();
+    }
+
+    public static String saveImage(Context context, View view, String filePath, String fileName, String imageFormat) throws IOException {
+        view.setDrawingCacheEnabled(true);
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache(true);
+
+        File dir = new File(filePath);
+        if(!dir.exists())
+            dir.mkdirs();
+        File file = new File(dir, fileName + imageFormat);
+        FileOutputStream fOut = new FileOutputStream(file);
+
+        Bitmap.createBitmap(view.getDrawingCache()).compress(Bitmap.CompressFormat.PNG, 100, fOut);
+        fOut.flush();
+        fOut.close();
+
+        return file.getAbsolutePath();
+    }
+}
